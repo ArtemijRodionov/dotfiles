1d923a9ea13f5dc33dfe45a6945ddeb89567e179
Artemiy Rodionov <wertins71@gmail.com>
2019-01-16 17:07:12 +0300

    Add osx related settigns to install.sh

--- install.sh
+++ install.sh
@@ -44,7 +44,7 @@ brew cask install \
 brew install bash-completion
 
 # Allow key repeats
-defaults write -g ApplePressAndHoldEnabled -bool false
+defaults write -g ApplePressAndHoldEnabled -bool true
 
 # View Hidden Files
 defaults write com.apple.Finder AppleShowAllFiles YES
@@ -52,21 +52,26 @@ defaults write com.apple.Finder AppleShowAllFiles YES
 # Automatically hide and show the Dock
 defaults write com.apple.dock autohide -bool true
 
-# Show Status bar in Finder"
+# Show Status bar in Finder
 defaults write com.apple.finder ShowStatusBar -bool true
 
-# Show Path bar in Finder"
+# Show Path bar in Finder
 defaults write com.apple.finder ShowPathbar -bool true
 
-# Set a blazingly fast keyboard repeat rate"
-defaults write NSGlobalDomain KeyRepeat -int 0.02
+# Set a blazingly fast keyboard repeat rate
+defaults write NSGlobalDomain KeyRepeat -int 2
+defaults write NSGlobalDomain InitialKeyRepeat -int 15
+defaults write NSGlobalDomain ApplePressAndHoldEnabled -int 0
 
 # Avoid creating .DS_Store files on network volumes"
 defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
 
-# Require password immediately after sleep or screen saver begins"
-defaults write com.apple.screensaver askForPassword -int 1
-defaults write com.apple.screensaver askForPasswordDelay -int 0
+# Require password immediately after sleep
+# defaults write com.apple.screensaver askForPassword -int 1
+# defaults write com.apple.screensaver askForPasswordDelay -int 0
+
+# Turn off annoying notification animation
+defaults write com.apple.dock no-bouncing -bool TRUE
 
 # Kill affected applications"
 for app in Safari Finder Dock Mail SystemUIServer; do killall "$app" >/dev/null 2>&1; done
